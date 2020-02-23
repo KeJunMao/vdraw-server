39185954c06069de16300e0bfdf9bcae4849dbb8
KeJun <kejun1997@gmail.com>
2020-02-23 16:47:04 +0800

    feat(server): 使用环境变量端口

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const app = require("express")();
 const http = require("http").Server(app);
 const io = require("socket.io")(http);
+const PORT = process.env.PORT || 3000;
 
 app.get("/", function(req, res) {
   res.send("Ok");
@@ -197,6 +198,6 @@ io.on("connection", socket => {
   });
 });
 
-http.listen(3000, () => {
-  console.log("listening on *:3000");
+http.listen(PORT, () => {
+  console.log(`listening on *:${PORT}`);
 });
